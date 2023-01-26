166e73b2585423405261c6fd86547f46796f1e90
DanishIsmail <danish.gakher8@gmail.com>
2023-01-26 16:20:50 +0500

    update the tests

--- test/token.js
+++ test/token.js
@@ -2,18 +2,47 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Token contract", () => {
-  it("Deployment should assign the total supply of token to the owner", async () => {
-    const [owner] = await ethers.getSigners(); //getting the owner
-    // console.log("owner", owner);
+  let Token;
+  let hardhatToken;
+  let owner;
+  let account1;
+  let account2;
+  let accounts;
 
-    const Token = await ethers.getContractFactory("Token"); //creating the token instance
+  beforeEach(async () => {
+    Token = await ethers.getContractFactory("Token"); //creating the token instance
 
-    const hardhatToken = await Token.deploy(); //deploying the contract
+    [owner, account1, account2, ...accounts] = await ethers.getSigners(); //getting the addresses
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
+    hardhatToken = await Token.deploy(); //deploying the contract
+  });
 
-    console.log("owner address", owner.address);
+  describe("Deployment", () => {
+    it("should assign the right owner", async () => {
+      expect(await hardhatToken.owner()).to.equal(owner.address);
+    });
 
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    it("should assign the total supply of token to the owner", async () => {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", async () => {
+    it("transfer the token to other address", async () => {
+      //transfer 20 token to account1
+      await hardhatToken.transfer(account1.address, 20);
+
+      let balanceOfAccount1 = await hardhatToken.balanceOf(account1.address);
+
+      expect(balanceOfAccount1).to.equal(20);
+
+      //transfer 10 token to account2
+      await hardhatToken.transfer(account2.address, 10);
+
+      let balanceOfAccount2 = await hardhatToken.balanceOf(account2.address);
+      expect(balanceOfAccount2).to.equal(10);
+    });
   });
 });
